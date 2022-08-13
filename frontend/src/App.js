66a828801b4c6f5fde33d220d9bb478c8a606c65
Zhdanov-Vyacheslav <slava.zhdanov@phkp.ru>
2022-08-13 19:40:16 +0300

    Created UserList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+import UserList from './components/UserList'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        'users': []
+    }
+  }
+
+  componentDidMount() {
+    axios
+        .get('http://localhost:8000/api/users/')
+        .then(response => {
+            const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+        })
+        .catch(error => console.log(error))
+  }
+
+    render() {
+     return(
+         <div>
+             <UserList users = {this.state.users} />
+         </div>
+     )
+  }
 }
 
 export default App;
